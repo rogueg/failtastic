fe2d09a63b342cee6bc2f35493168815a59b0e27
grant <grant@meraki.com>
2013-03-06 19:41:06 -0800

    add astats page, which has failing tests by team, and over 6 months.

--- app/controllers/fail_controller.rb
+++ app/controllers/fail_controller.rb
@@ -12,4 +12,23 @@ class FailController < ApplicationController
     @old_replies = @fal.human_replies.where("sent_at < ?", @curr_fail.started_at)
   end
 
+  def stats
+    recent = Failure.recent.to_a
+
+    @by_team = recent.group_by(&:team).map_values do |team, failures|
+      Array.wrap(failures).group_by(&:status).map_values(&:length)
+    end
+
+    failures = Failure.connection.execute <<-EOF
+      select strftime('%s', started_at) as start, strftime('%s', ended_at) as end
+      from failures where started_at > datetime('now', '-6 Month')
+    EOF
+    @fail_by_day = (6.months.ago.beginning_of_day.to_i..Time.now.to_i).step(1.day).inject({}) {|h,i| h[i] = 0; h}
+    failures.each{|f|
+      @fail_by_day.each{|d,v|
+        @fail_by_day[d] = v+1 if f['start'].to_i < d && f['end'].to_i > d
+      }
+    }
+    @fail_by_day = @fail_by_day.map{|d,cnt| {:day => d, :count => cnt}}.sort_by{|v| v[:day]}
+  end
 end
